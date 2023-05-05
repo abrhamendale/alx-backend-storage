7ca6715708a5a08b3ac6a84f180a4bc660fd3fcd
Abrham Endale <tmailae2020@gmail.com>
2023-05-05 17:51:06 +0300

    Fixed 7-average_score.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,9 +4,8 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE a INT DEFAULT user_id;
-	SELECT @b := AVG(score) FROM corrections WHERE corrections.user_id = a;
 	UPDATE users
-	SET average_score = @b
+	SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = a)
 	WHERE id = user_id;
 END $$
 DELIMITER $$
