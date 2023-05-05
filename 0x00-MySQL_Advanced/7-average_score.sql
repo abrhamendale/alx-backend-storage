45ac9d249f74f09118ae7bae04ecf14eb288bcbe
Abrham Endale <tmailae2020@gmail.com>
2023-05-05 14:21:44 +0300

    A fix for 7-average_score.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,12 +1,11 @@
---Creates a stored procedure AddBonus
+-- Creates a stored procedure AddBonus
 
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	SET @a = user_id;
 	UPDATE users
-	SET average_score = (SELECT SUM(score) FROM corrections WHERE user_id = @a) 
-		/ (SELECT COUNT(score) FROM corrections WHERE user_id= @a)
+	SET average_score = SELECT AVG(score) FROM corrections WHERE user_id = @a
 	WHERE id = user_id;
 END $$
 DELIMITER $$
