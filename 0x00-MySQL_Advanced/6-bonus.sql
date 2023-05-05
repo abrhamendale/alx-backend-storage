228f8d5a4f2404b06b746c9459b2a143d7e012cf
Abrham Endale <tmailae2020@gmail.com>
2023-05-05 13:52:12 +0300

    Fixed 6-bonus.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score
 BEGIN
 	SET @a = user_id;
 	SET @b = score;
+	SELECT DISTINCT @C := id FROM projects WHERE name = project_name;
 	IF (EXISTS(SELECT name FROM projects WHERE name = project_name))
 		THEN
 		INSERT INTO corrections (user_id, project_id, score) 
-			VALUES (@a, (SELECT DISTINCT project_id FROM projects 
-				WHERE name = project_name), @b);
+			VALUES (@a, @c, @b);
 	ELSE
 		INSERT INTO projects (name) VALUES (project_name);
 		INSERT INTO corrections (user_id, project_id, score)
