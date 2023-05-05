24a9f8baadbd34112af585a5243e2cfe530778cf
Abrham Endale <tmailae2020@gmail.com>
2023-05-05 15:10:31 +0300

    Fixed 7-compute_average.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,12 +1,12 @@
 -- Creates a stored procedure AddBonus
 
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN u_id INT)
 BEGIN
-	SET @a = user_id;
-	SELECT @b := AVG(score) FROM corrections WHERE user_id = @a;
+	SET @a = u_id;
+	SELECT @b := AVG(score) FROM corrections WHERE user_id = u_id;
 	UPDATE users
 	SET average_score = @b
-	WHERE id = user_id;
+	WHERE id = u_id;
 END $$
 DELIMITER $$
