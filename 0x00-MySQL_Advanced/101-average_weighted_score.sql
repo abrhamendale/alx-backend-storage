e48904de195e55f4455420e65346bc54743b3426
Abrham Endale <tmailae2020@gmail.com>
2023-05-06 23:05:01 +0300

    Added averagescoreforusers.

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,10 +3,13 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	SELECT corrections.user_id AS id, users.name, (users.SUM(corrections.score * projects.weight) / SUM(projects.weight)) AS average_score
-	FROM corrections
-		JOIN projects ON corrections.project_id = projects.id
-		JOIN users ON corrections.user_id = users.id
-	GROUP BY corrections.user_id;
+	SET @a =
+	(SELECT (SUM(corrections.score * projects.weight) / SUM(projects.weight))
+		    FROM corrections
+		    JOIN projects ON corrections.project_id = projects.id
+		    JOIN users ON corrections.user_id = users.id and corrections.user_id = user_id
+		GROUP BY corrections.user_id);
+	UPDATE users
+	        SET average_score = @a;
 END $$
 DELIMITER $$
