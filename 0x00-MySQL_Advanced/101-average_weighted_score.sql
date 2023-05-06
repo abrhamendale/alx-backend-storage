6a23c50e29a7da60e2fb6be046d92226b09785a9
Abrham Endale <tmailae2020@gmail.com>
2023-05-06 23:06:27 +0300

    Added averagescoreforusers.

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,7 +7,7 @@ BEGIN
 	(SELECT (SUM(corrections.score * projects.weight) / SUM(projects.weight))
 		    FROM corrections
 		    JOIN projects ON corrections.project_id = projects.id
-		    JOIN users ON corrections.user_id = users.id and corrections.user_id = user_id
+		    JOIN users ON corrections.user_id = users.id
 		GROUP BY corrections.user_id);
 	UPDATE users
 	        SET average_score = @a;
