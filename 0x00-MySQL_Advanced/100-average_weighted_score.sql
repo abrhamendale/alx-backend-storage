3ec1d13f84718949f6c9053db0676cc79c247ea9
Abrham Endale <tmailae2020@gmail.com>
2023-05-06 23:02:28 +0300

    Added averagescoreforuser.

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,15 +3,14 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE c INT DEFAULT user_id;
-	SELECT @a := (corrections.score * projects.weight) / SUM(projects.weight)
-	FROM corrections
-	JOIN projects 
-	ON corrections.project_id = projects.id
-	WHERE corrections.user_id = c
-	GROUP BY corrections.user_id;
+	SET @a =
+	(SELECT (SUM(corrections.score * projects.weight) / SUM(projects.weight))
+		    FROM corrections
+		    JOIN projects ON corrections.project_id = projects.id
+		    JOIN users ON corrections.user_id = users.id and corrections.user_id = user_id
+		GROUP BY corrections.user_id);
 	UPDATE users
-	SET average_score = @a
-	WHERE id = user_id;
+	        SET average_score = @a
+		WHERE users.id = user_id;
 END $$
 DELIMITER $$
--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,7 +3,7 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	SELECT corrections.user_id, users.name, users.SUM(corrections.score * projects.weight) / SUM(projects.weight)
+	SELECT corrections.user_id AS id, users.name, (users.SUM(corrections.score * projects.weight) / SUM(projects.weight)) AS average_score
 	FROM corrections
 		JOIN projects ON corrections.project_id = projects.id
 		JOIN users ON corrections.user_id = users.id
