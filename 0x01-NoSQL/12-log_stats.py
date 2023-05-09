83d8cd9ae3028388d6c9f014088a38c0a01ce422
Abrham Endale <abrhamendale9@gmail.com>
2023-05-09 11:17:17 -0700

    A fix for task_12.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -14,11 +14,11 @@ def log_parser():
     n = client.logs.nginx
     print(n.count_documents({}), "logs")
     print("Methods:")
-    print("\t", "method GET:", n.count_documents({"method" : "GET"}))
-    print("\t", "method POST:", n.count_documents({"method" : "POST"}))
-    print("\t", "method PUT:", n.count_documents({"method" : "PUT"}))
-    print("\t", "method PATCH:", n.count_documents({"method" : "PATCH"}))
-    print("\t", "method DELETE:", n.count_documents({"method" : "DELETE"}))
+    print("   ", "method GET:", n.count_documents({"method" : "GET"}))
+    print("   ", "method POST:", n.count_documents({"method" : "POST"}))
+    print("   ", "method PUT:", n.count_documents({"method" : "PUT"}))
+    print("   ", "method PATCH:", n.count_documents({"method" : "PATCH"}))
+    print("   ", "method DELETE:", n.count_documents({"method" : "DELETE"}))
     print(n.count_documents({ '$and': [{"method" : "GET"}, {"path" : "/status"}]}),"status check")
 
 
