224b2e305588f79b9b6fe17983e0904ad181b7e5
Abrham Endale <tmailae2020@gmail.com>
2023-05-09 19:54:33 +0300

    Added task_14.

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,6 +4,9 @@ pyMongo
 """
 
 
+import pymongo
+
+
 def top_students(mongo_collection):
     """Returns all students sorted by average score."""
     return (mongo_collection.aggregate([{$group: {"_id": "_id", averageScore: { $avg: "$score" }}}]))
