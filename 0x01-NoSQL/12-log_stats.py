5c2865428e8cf5f32a6144031dbeb5a3563ab323
Abrham Endale <abrhamendale9@gmail.com>
2023-05-09 11:03:53 -0700

    Fixed task_12.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,11 +15,11 @@ def log_parser():
     print(n.count_documents({}), " logs")
     print("Methods:")
     print("\t", "method GET: ", n.count_documents({"method" : "GET"}))
-    print("\t", "method GET: ", n.count_documents({"method" : "POST"}))
-    print("\t", "method GET: ", n.count_documents({"method" : "PUT"}))
-    print("\t", "method GET: ", n.count_documents({"method" : "PATCH"}))
-    print("\t", "method GET: ", n.count_documents({"method" : "DELETE"}))
-    print("\t", n.count_documents({ '$and': [{"method" : "GET"}, {"path" : "/status"}]})," status check")
+    print("\t", "method POST: ", n.count_documents({"method" : "POST"}))
+    print("\t", "method PUT: ", n.count_documents({"method" : "PUT"}))
+    print("\t", "method PATCH: ", n.count_documents({"method" : "PATCH"}))
+    print("\t", "method DELETE: ", n.count_documents({"method" : "DELETE"}))
+    print(n.count_documents({ '$and': [{"method" : "GET"}, {"path" : "/status"}]})," status check")
 
 
 if __name__ == "__main__":
