862cad2fcf580ca8eb6805514f2f7129c5e3495d
Abrham Endale <abrhamendale9@gmail.com>
2023-05-09 13:10:22 -0700

    A fix for tasks_9 and 11.

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,4 +8,5 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts items into collection."""
-    return (mongo_collection.insert_one(kwargs))
+    if mongo_collection is not None:
+        return (mongo_collection.insert_one(kwargs))
